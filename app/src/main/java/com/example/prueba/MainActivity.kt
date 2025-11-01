779449b74ea1f9527459aee93e71190209e2a554
Piero <piero.villanueva20@gmail.com>
2025-11-01 03:45:58 -0500

    Commit inicial - Interfaz "Menú"

---
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.prueba
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rvProductos = findViewById<RecyclerView>(R.id.rvProductosRecientes)
+        rvProductos.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        val lista = listOf(
+            Producto("Logitech GT12", "Mouse Wireless", 345.0, R.drawable.img_mouse),
+            Producto("Monitor Ocelot Gaming OM-C32 ", "Monitor Gaming", 569.00, R.drawable.img_monitor),
+            Producto("Logitech G15 RGB", "Wireless Keyboard", 479.0, R.drawable.img_teclado),
+            Producto("Vsg Aguila", "Mouse Gaming", 69.0, R.drawable.img_mouse2),
+            Producto("Game Extreme", "Silla Gamer", 699.0, R.drawable.img_silla),
+            Producto("Logitech GT12", "Mouse Wireless", 345.0, R.drawable.img_mouse),
+            Producto("Monitor Ocelot Gaming OM-C32 ", "Monitor Gaming", 569.00, R.drawable.img_monitor),
+            Producto("Logitech G15 RGB", "Wireless Keyboard", 479.0, R.drawable.img_teclado),
+            Producto("Game Extreme", "Silla Gamer", 699.0, R.drawable.img_silla),
+            Producto("Vsg Aguila", "Mouse Gaming", 69.0, R.drawable.img_mouse2)
+        )
+
+        rvProductos.adapter = ProductoAdapter(lista)
+    }
+}
---
+++ app/src/main/java/com/example/prueba/Producto.kt
@@ -0,0 +1,9 @@
+package com.example.prueba
+
+
+data class Producto(
+    val nombre: String,
+    val descripcion: String,
+    val precio: Double,
+    val imagen: Int
+)
